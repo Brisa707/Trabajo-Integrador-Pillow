fe8eb765745ab7170127ee9de3411d126d685175
Brisa Valero <138631888+Brisa707@users.noreply.github.com>
2024-06-30 23:24:37 -0300

    Delete Agrandado.py

--- Agrandado.py
+++
@@ -1,6 +0,0 @@
-
-def cambiar_tamano(imagen):
-    Porcentaje = int(input("Ingrese un porcentaje: "))
-    nueva_imagen = imagen.resize((int(imagen.width * Porcentaje / 100), int(imagen.height * Porcentaje / 100)))
-    print("Tamaño de imagen cambiado.")
-    return nueva_imagen
